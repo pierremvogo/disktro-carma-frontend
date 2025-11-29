8737e93ca10a0666efb0a031f9d043290773ea61
pnkolom <mvogopierre129@gmail.com>
2025-11-29 10:52:45 +0100

    new commit

--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -1,8 +1,8 @@
 import ResetPasswordForm from "@/modules/auth/resetPassword";
 import React from "react";
 
-const page = () => {
-  return <ResetPasswordForm />;
+const page = ({ searchParams }: { searchParams: { token?: string } }) => {
+  return <ResetPasswordForm initialToken={searchParams.token || ""} />;
 };
 
 export default page;
