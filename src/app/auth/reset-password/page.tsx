038f2346ab27ddfb75ee6256b82523572aca65ab
pnkolom <mvogopierre129@gmail.com>
2026-02-07 12:07:09 +0100

    new commit

--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -1,24 +1,28 @@
 "use client";
-
 import ResetPasswordForm from "@/modules/auth/resetPassword";
-import { useSearchParams } from "next/navigation";
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 type Language = "english" | "spanish" | "catalan";
 
-export default function Page() {
-  const searchParams = useSearchParams();
-
-  const token = useMemo(() => searchParams.get("token") ?? "", [searchParams]);
-
+const Page = ({ searchParams }: { searchParams: { token?: string } }) => {
   const [language, setLanguage] = useState<Language>("english");
 
   useEffect(() => {
     const storedLanguage = localStorage.getItem(
       "disktro_language"
     ) as Language | null;
-    if (storedLanguage) setLanguage(storedLanguage);
+
+    if (storedLanguage) {
+      setLanguage(storedLanguage);
+    }
   }, []);
 
-  return <ResetPasswordForm initialToken={token} language={language} />;
-}
+  return (
+    <ResetPasswordForm
+      initialToken={searchParams.token || ""}
+      language={language}
+    />
+  );
+};
+
+export default Page;
